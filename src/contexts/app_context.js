94b15fc386692c685ba60d50f87b3353651443e5
Lynette <lynettecargo@Lynettes-Air.home>
2023-07-18 16:13:27 -0500

    useContext Attempt

--- src/contexts/app_context.js
+++ src/contexts/app_context.js
@@ -1,1 +1,11 @@
 import { useState, createContext } from "react";
+import employeeList from '../model/employeeList'
+import personnel from "../model/employeeList";
+
+export const WorkerContext = createContext();
+
+function EmployeeContextProvider(props){
+    const [person, setWorker] = useState(personnel[0]);
+    const [ employee, setEmployee] = useState(personnel);
+    
+}
